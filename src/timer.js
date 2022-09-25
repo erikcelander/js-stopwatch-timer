8d36ba650d595b96ffdbce134f968286c92a92f7
Erik Kroon Celander <ek223ur@student.lnu.se>
2022-09-25 15:52:53 +0200

    Make fields private

--- src/timer.js
+++ src/timer.js
@@ -1,19 +1,19 @@
 export class Timer {
 
-  ms
-  seconds
-  minutes
-  hours
-  days
-  timer
+  #ms
+  #seconds
+  #minutes
+  #hours
+  #days
+  #timer
 
   constructor () {
-    this.ms = 0
-    this.seconds = 0
-    this.minutes = 0
-    this.hours = 0
-    this.days = 0
-    this.timer
+    this.#ms = 0
+    this.#seconds = 0
+    this.#minutes = 0
+    this.#hours = 0
+    this.#days = 0
+    this.#timer
   }
 
 }
